a36e5851acc8a77db72e7ca7041968e5efe1ba9b
Bartosz Lokaj <bartoszlokaj@gmail.com>
2019-08-20 15:33:16 +0200

    initial commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+//  ICONS
+////////////////////////////////////
+
+
+//  IMAGES
+////////////////////////////////////
+import hero1 from "./img/luca-bravo-photo.jpg";
+import hero2 from "./img/v2osk-photo.jpg";
+import hero3 from "./img/frank-mckenna-photo.jpg";
+import hero4 from './img/filippos-sdralias-photo.jpg';
+
+//  FUNCTIONS
+////////////////////////////////////////
+
+
+const scss = require('./sass/main.scss');
+
+console.log('Hello from app.js');
---
+++ webpack.config.js
@@ -0,0 +1,100 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "app.bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "./dist",
+              hmr: process.env.NODE_ENV === "development"
+            }
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              url: false
+            }
+          },
+          // "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "img/",
+              publicPath: "img/"
+            }
+          },
+          {
+            loader: "image-webpack-loader",
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false
+              },
+              pngquant: {
+                quality: "65-90",
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false
+              },
+              // the webp option will enable WEBP
+
+            }
+          }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    stats: "errors-only",
+    open: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Bluepress",
+      minify: {
+        collapseWhitespace: true
+      },
+      hash: true,
+      template: "./src/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "app.css"
+    })
+  ]
+};
